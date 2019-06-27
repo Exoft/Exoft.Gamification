dbc5f96186b398e9fc9b3216b44b9b9f03639c32
OstapRoik <ostap2308@gmail.com>
2019-06-27 16:33:46 +0300

    Fixed bug with email

--- Exoft.Gamification.Api.Services/EmailService.cs
+++ Exoft.Gamification.Api.Services/EmailService.cs
@@ -9,10 +9,13 @@ namespace Exoft.Gamification.Api.Services
     {
         public async Task SendEmailAsync(string email, string subject, string message)
         {
+            string emailAdmin = "yelyzaveta.brednieva.pz.2016@lpnu.ua";
+            string passwordAdmin = "03.02.1999";
+
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("Exoft", "yelyzaveta.brednieva.pz.2016@lpnu.ua"));
-            emailMessage.To.Add(new MailboxAddress("", "ostap2308@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress("Exoft", emailAdmin));
+            emailMessage.To.Add(new MailboxAddress("", email));
             emailMessage.Subject = subject;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
             {
@@ -23,7 +26,7 @@ namespace Exoft.Gamification.Api.Services
             {
                 await client.ConnectAsync("smtp.gmail.com", 465, true);
 
-                await client.AuthenticateAsync("yelyzaveta.brednieva.pz.2016@lpnu.ua", "03.02.1999");
+                await client.AuthenticateAsync(emailAdmin, passwordAdmin);
 
                 await client.SendAsync(emailMessage);
 
