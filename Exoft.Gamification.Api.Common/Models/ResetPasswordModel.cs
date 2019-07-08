cd54dc1605d8d03ef150a349785b37df06b2573b
OstapRoik <ostap2308@gmail.com>
2019-07-08 17:03:55 +0300

    Added NonLogged attribute and logic for exclude properties

--- Exoft.Gamification.Api.Common/Models/ResetPasswordModel.cs
+++ Exoft.Gamification.Api.Common/Models/ResetPasswordModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Exoft.Gamification.Api.Common.Models
+﻿using Exoft.Gamification.Api.Common.Helpers;
+
+namespace Exoft.Gamification.Api.Common.Models
 {
     public class ResetPasswordModel
     {
+        [NonLogged]
         public string Password { get; set; }
 
         public string SecretString { get; set; }
--- Exoft.Gamification.Api/Helpers/ErrorHandlingFilter.cs
+++ Exoft.Gamification.Api/Helpers/ErrorHandlingFilter.cs
@@ -1,20 +1,20 @@
-﻿using Microsoft.AspNetCore.Http.Internal;
+﻿using Exoft.Gamification.Api.Common.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Exoft.Gamification.Api.Helpers
 {
     public class ErrorHandlingFilter : ExceptionFilterAttribute
     {
-        private static readonly List<string> propertiesForExcluding = new List<string>()
-        {
-            "password"
-        };
-
         private readonly ILogger<ErrorHandlingFilter> _logger;
 
         public ErrorHandlingFilter
@@ -33,6 +33,8 @@ namespace Exoft.Gamification.Api.Helpers
             stringBuilder.Append(string.Format("\r\n ---> Params: {0}",
                 string.Join("\r\n\t -->", context.RouteData.Values)));
 
+            
+            // if body
             context.HttpContext.Request.EnableRewind();
             context.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
 
@@ -42,22 +44,55 @@ namespace Exoft.Gamification.Api.Helpers
                 if(!string.IsNullOrEmpty(body))
                 {
                     var jsonBody = JObject.Parse(body);
-                    foreach (var propertyForExlude in propertiesForExcluding)
-                    {
-                        jsonBody.Remove(propertyForExlude);
-                    }
-                    jsonBody.Remove("password");
-                    stringBuilder.Append(string.Format("\r\n ---> Body: {0}", jsonBody.ToString()));
+
+                    var clearJson = RemoveExcludedProperties(jsonBody, context);
+                    
+                    stringBuilder.Append(string.Format("\r\n ---> Body: {0} \r\n", clearJson.ToString()));
                 }
             }
             
+            // if form
             if(context.HttpContext.Request.HasFormContentType)
             {
-                stringBuilder.Append(string.Format("\r\n ---> Form: {0} \r\n", string.Join("\r\n\t -->", 
-                    context.HttpContext.Request.Form)));
+                var form = context.HttpContext.Request.Form;
+                var jsonForm = ToJSON(form);
+
+                var clearJson = RemoveExcludedProperties(jsonForm, context);
+
+                stringBuilder.Append(string.Format("\r\n ---> Form: {0} \r\n", clearJson.ToString()));
             }
 
             _logger.LogError(context.Exception, stringBuilder.ToString());
+        }
+
+        private JObject RemoveExcludedProperties(JObject json, ExceptionContext context)
+        {
+            var controllerName = context.RouteData.Values["controller"].ToString() + "Controller";
+            var controllerType = Type.GetType("Exoft.Gamification.Api.Controllers." + controllerName);
+            var actionName = context.RouteData.Values["action"].ToString();
+            var method = controllerType.GetMethod(actionName);
+            var parameters = method.GetParameters()[0];
+            var modelType = parameters.ParameterType;
+            var excludedProperties = modelType.GetProperties().Where(prop =>
+                                        IsDefined(prop, typeof(NonLoggedAttribute)));
+            
+            foreach (var propertyForExlude in excludedProperties)
+            {
+                json.Remove(propertyForExlude.Name.ToLower());
+            }
+
+            return json;
+        }
+        
+        private JObject ToJSON(IFormCollection formCollection)
+        {
+            var dictionary = new Dictionary<string, string>();
+            foreach (string key in formCollection.Keys)
+            {
+                dictionary.Add(key, formCollection[key]);
+            }
+
+            return JObject.Parse(JsonConvert.SerializeObject(dictionary));
         }
     }
 }
