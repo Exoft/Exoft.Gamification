7155752fea7080085d7c2e31bd1f88c1d44d31f5
OstapRoik <ostap2308@gmail.com>
2019-07-09 13:57:24 +0300

    Refactor ErrorHandlingFilter

--- Exoft.Gamification.Api/Helpers/ErrorHandlingFilter.cs
+++ Exoft.Gamification.Api/Helpers/ErrorHandlingFilter.cs
@@ -1,11 +1,12 @@
 ﻿using Exoft.Gamification.Api.Common.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -38,28 +39,28 @@ namespace Exoft.Gamification.Api.Helpers
             context.HttpContext.Request.EnableRewind();
             context.HttpContext.Request.Body.Seek(0, SeekOrigin.Begin);
 
+            JObject jsonData = new JObject();
             using (StreamReader stream = new StreamReader(context.HttpContext.Request.Body))
             {
                 string body = stream.ReadToEnd();
                 if(!string.IsNullOrEmpty(body))
                 {
-                    var jsonBody = JObject.Parse(body);
-
-                    var clearJson = RemoveExcludedProperties(jsonBody, context);
-                    
-                    stringBuilder.Append(string.Format("\r\n ---> Body: {0} \r\n", clearJson.ToString()));
+                    jsonData = JObject.Parse(body);
                 }
             }
-            
+
             // if form
-            if(context.HttpContext.Request.HasFormContentType)
+            if (context.HttpContext.Request.HasFormContentType)
             {
                 var form = context.HttpContext.Request.Form;
-                var jsonForm = ToJSON(form);
+                jsonData = ToJSON(form);
+            }
+            
+            if (jsonData.Count != 0)
+            {
+                var clearJson = RemoveExcludedProperties(jsonData, context);
 
-                var clearJson = RemoveExcludedProperties(jsonForm, context);
-
-                stringBuilder.Append(string.Format("\r\n ---> Form: {0} \r\n", clearJson.ToString()));
+                stringBuilder.Append(string.Format("\r\n ---> Data: {0} \r\n", clearJson.ToString()));
             }
 
             _logger.LogError(context.Exception, stringBuilder.ToString());
@@ -67,18 +68,30 @@ namespace Exoft.Gamification.Api.Helpers
 
         private JObject RemoveExcludedProperties(JObject json, ExceptionContext context)
         {
-            var controllerName = context.RouteData.Values["controller"].ToString() + "Controller";
-            var controllerType = Type.GetType("Exoft.Gamification.Api.Controllers." + controllerName);
+            var controllerType = (context.ActionDescriptor as ControllerActionDescriptor).ControllerTypeInfo;
             var actionName = context.RouteData.Values["action"].ToString();
             var method = controllerType.GetMethod(actionName);
-            var parameters = method.GetParameters()[0];
+            var parameters = method.GetParameters().SingleOrDefault(param =>
+                        param.IsDefined(typeof(FromBodyAttribute), false) ||
+                        param.IsDefined(typeof(FromFormAttribute), false));
+
             var modelType = parameters.ParameterType;
             var excludedProperties = modelType.GetProperties().Where(prop =>
                                         IsDefined(prop, typeof(NonLoggedAttribute)));
-            
+
+            if(excludedProperties.Count() == 0)
+            {
+                return json;
+            }
+
             foreach (var propertyForExlude in excludedProperties)
             {
-                json.Remove(propertyForExlude.Name.ToLower());
+                var jToken = json.GetValue(propertyForExlude.Name, System.StringComparison.OrdinalIgnoreCase);
+
+                if(jToken != null)
+                {
+                    json.Remove(jToken.Path);
+                }
             }
 
             return json;
